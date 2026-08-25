e7dfecd42acaa50b030ddeb30e6b64500c5c4e12
Nic <balln13572@gmail.com>
2026-08-24 21:26:18 -0600

    added dblclick event

--- src/dom/events/mod.rs
+++ src/dom/events/mod.rs
@@ -248,6 +248,7 @@ macro_rules! handler {
 
 pub mod ev {
     handler! { click, "click", web_sys::PointerEvent }
+    handler! { dbl_click, "dblclick", web_sys::MouseEvent }
     handler! { submit, "submit", web_sys::SubmitEvent }
     handler! { pointer_down, "pointerdown", web_sys::PointerEvent }
     handler! { pointer_move, "pointermove", web_sys::PointerEvent }
